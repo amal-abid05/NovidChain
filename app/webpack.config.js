dc541d5e285b5f419c21b52354edee7bed282296
Amal Abid <amal.abid05@gmail.com>
2021-07-10 15:40:14 +0100

    changes

---
+++ app/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  entry: './lib/index.js',
+  output: {
+    filename: 'uport-credentials.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    library: 'Uport Credentials'
+  }
+}
